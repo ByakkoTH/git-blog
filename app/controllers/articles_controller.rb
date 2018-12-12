edd015f476ffbc945bdfab0daff4367b7adad093
Martin <argamartin1922@gmail.com>
2018-12-12 12:27:55 -0300

    modificado

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,23 @@
 class ArticlesController < ApplicationController
+    def index
+        @article=Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
     end
+
     def create
-        render plain: params[:articles]
+        @article=Article.new(article_params)
+        @article.save
+        redirect_to @article
+    end
+
+    private
+    def article_params
+        params.require(:articles).permit(:title, :text)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  resources :articles
+ get "say" => "say#hello"
 end
