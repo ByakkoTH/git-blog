196dab423b4c765ccc35b60417a7ee729ea3aff5
byakko <hilezarcoanbu@gmail.com>
2018-12-14 13:34:43 -0300

    5.12 partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,29 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
         @article=Article.new(article_params)
-        @article.save
+       if @article.save
         redirect_to @article
+       else
+        render "new"#me lleva a la plantilla de new
+       end
+    end
+
+    def update
+        @article = Article.finde(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render "edit"
+        end
     end
 
     private
